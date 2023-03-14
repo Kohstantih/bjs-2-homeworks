8267a5f9cf92186a4c272e12ca95c2c240e8c549
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2023-03-14 15:54:04 +0200

    Task1 base-concepts copleted

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d;
+  d = b**2 - 4 * a * c;
+  if (d < 0) {
+    
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d) ) / (2 * a));
+  };
   return arr;
-}
+};
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false
+  } else {
+    let S, P, n, monthPay, sum;
+    P = (percent / 100) / 12;
+    S = amount - contribution;
+    n = countMonths;
+    monthPay = S * (P + (P / (((1 + P)**n) - 1)));
+    sum = Number((n * monthPay).toFixed(2));
+    return sum;
+  };
+};
