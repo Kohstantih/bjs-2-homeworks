e55144c03bef49a05a5232bddbb20e9a41a05820
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2023-04-05 20:14:04 +0300

    Task 3.arrays comleted

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    return (arr1.length === arr2.length && arr1.every(i => arr1[i] === arr2[i]));
+};
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    return users.filter(user => user.gender === gender).reduce((acc, item, index, Array) => acc += item.age / Array.length, 0);
+};
