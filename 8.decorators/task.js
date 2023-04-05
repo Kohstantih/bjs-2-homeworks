2e4d86571f50b3e8757d5ded254f8118c7d8d03d
Konstantin <117026423+Kohstantih@users.noreply.github.com>
2023-04-05 20:01:56 +0300

    Task 8.decorators comleted

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,48 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    const maxCacheValuesCount = 5;
+    return (...args) => {
+      const hash = md5(args);
+      const objectInCache = cache.find((item) => item[hash]);
+      if (objectInCache) {
+        console.log("Из кэша: " + objectInCache[hash]);
+        return 'Из кэша: ' + objectInCache[hash];
+      };
+
+      let result = func(...args);
+      cache.push({[hash]: result});
+      if (cache.length > maxCacheValuesCount) {
+        cache.shift();
+      };
+
+      console.log("Вычисляем: " + result);
+      return 'Вычисляем: ' + result;
+    };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
-}
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+      
+    function wrapper(...args) {
+       wrapper.allCount++;
+       if (!timeoutId) {
+           func(...args);
+           wrapper.count++;                
+      };
+        
+      if (timeoutId) {
+          clearTimeout(timeoutId);
+      };
+        
+      timeoutId = setTimeout(() => {        
+          wrapper.count++;
+          return func(...args);
+      }, delay);  
+    };
+    
+    return wrapper;
+}ж
